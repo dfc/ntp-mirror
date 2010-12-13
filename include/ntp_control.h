be636627da100715791a74e364114a5c513d0327
Harlan Stenn <stenn@ntp.org>
2010-12-13 03:28:45 -0500

    * [Bug 1510] from 4.2.6p3-RC12: Add modes 20/21 for driver 8 to support   RAWDCF @ 75 baud. * [Bug 1741] from 4.2.6p3-RC12: Enable multicast reception on each   address (Windows). * from 4.2.6p3-RC12: Other manycastclient repairs:   Separate handling of scope ID embedded in many in6_addr from ifindex   used for IPv6 multicasting ioctls.   Add INT_PRIVACY endpt bit flag for IPv6 RFC 4941 privacy addresses.   Enable outbound multicast from only one address per interface in the   same subnet, and in that case prefer embedded MAC address modified   EUI-64 IPv6 addresses first, then static, and last RFC 4941 privacy   addresses.   Use setsockopt(IP[V6]_MULTICAST_IF) before each send to multicast to   select the local source address, using the correct socket is not   enough. * "server ... ident <groupname>" changes from Dave Mills. * Documentation updates from Dave Mills. * [Bug 1510] Add modes 20/21 for driver 8 to support RAWDCF @ 75 baud. * [Bug 1741] Enable multicast reception on each address (Windows). * Other manycastclient repairs:   Separate handling of scope ID embedded in many in6_addr from ifindex   used for IPv6 multicasting ioctls.   Add INT_PRIVACY endpt bit flag for IPv6 RFC 4941 privacy addresses.   Enable outbound multicast from only one address per interface in the   same subnet, and in that case prefer embedded MAC address modified   EUI-64 IPv6 addresses first, then static, and last RFC 4941 privacy   addresses.   Use setsockopt(IP[V6]_MULTICAST_IF) before each send to multicast to   select the local source address, using the correct socket is not   enough.

--- include/ntp_control.h
+++ include/ntp_control.h
@@ -18,7 +18,7 @@ struct ntp_control {
 /*
  * Length of the control header, in octets
  */
-#define	CTL_HEADER_LEN		12
+#define	CTL_HEADER_LEN		(offsetof(struct ntp_control, data))
 #define	CTL_MAX_DATA_LEN	468
 
 
@@ -261,7 +261,8 @@ struct ntp_control {
 #define	CP_INITKEY		(5 + CP_MAX_NOAUTOKEY)
 #define	CP_INITTSP		(6 + CP_MAX_NOAUTOKEY)
 #define	CP_SIGNATURE		(7 + CP_MAX_NOAUTOKEY)
-#define	CP_MAXCODE		CP_SIGNATURE
+#define	CP_IDENT		(8 + CP_MAX_NOAUTOKEY)
+#define	CP_MAXCODE		CP_IDENT
 #else	/* !AUTOKEY follows */
 #define	CP_MAXCODE		CP_MAX_NOAUTOKEY
 #endif	/* !AUTOKEY */
--- sntp/version.m4
+++ sntp/version.m4
@@ -1,1 +1,1 @@
-m4_define([VERSION_NUMBER],[4.2.7p92])
+m4_define([VERSION_NUMBER],[4.2.7p93])
